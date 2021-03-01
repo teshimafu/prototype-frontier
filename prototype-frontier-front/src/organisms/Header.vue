459f13f942102fe8f40cfaefb35633290883d6cb
teshimafu <teshima.fu@gmail.com>
2021-03-01 11:37:30 +0900

    アプリタイトル修正及びフォルダ名変更 (#11)
    * フォルダ名修正
    
    * タイトル変更。説明文追加

---
+++ prototype-frontier-front/src/organisms/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <h1 id="title">Prototype Frontier</h1>
+    <div id="abstruct">
+      自分の作ったアプリケーションを投稿してみんなに見てもらいましょう<br />
+      将来的にtwitterでランダムにアプリを投稿する機能も追加予定です
+    </div>
+  </div>
+</template>
+<style scoped>
+.header {
+  background-color: aquamarine;
+}
+
+#title {
+  padding: 10px;
+  margin: 0;
+  font-weight: bold;
+  font-style: oblique;
+}
+
+.header > #abstruct {
+  background-color: rgba(255, 255, 255, 0.8);
+}
+</style>

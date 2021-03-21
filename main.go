94a5bc62adfed03f97f54d7d139f056f03175bc6
teshimafucd <teshima.fu@gmail.com>
2021-03-22 02:23:55 +0900

    gormからgocraft/dbrに修正

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/lib/pq"
 	"local.packages/server"
 )
 
--- server/portfolioService.go
+++ server/portfolioService.go
@@ -5,68 +5,73 @@ import (
 )
 
 func getPortfolio(ID int) (Portfolio, error) {
-	db, err := gormConnect()
+	sess, err := gocraftConnection()
 	if err != nil {
 		return Portfolio{}, err
 	}
-	defer db.Close()
 
 	portfolio := Portfolio{}
-
-	err = db.Find(&portfolio, ID).Error
-	if err != nil {
-		return Portfolio{}, err
-	}
-	return portfolio, nil
+	err = sess.Select("*").From("portfolio").Where("id = ?", ID).LoadOne(&portfolio)
+	return portfolio, err
 }
 
 func getPortfolioList() ([](Portfolio), error) {
-	db, err := gormConnect()
+	sess, err := gocraftConnection()
 	if err != nil {
-		return [](Portfolio){}, err
+		return []Portfolio{}, err
 	}
-	defer db.Close()
 
-	portfolioList := [](Portfolio){}
-
-	db.Order("created_at desc").Find(&portfolioList)
+	var portfolioList []Portfolio
+	sess.Select("*").From("portfolio").OrderDesc("id").Load(&portfolioList)
 	return portfolioList, nil
 }
 
 func postPortfolio(p *Portfolio) (Portfolio, error) {
-
-	db, err := gormConnect()
+	sess, err := gocraftConnection()
 	if err != nil {
 		return Portfolio{}, err
 	}
-	defer db.Close()
-
 	p.CreatedAt = time.Now()
-	db.Create(p)
+	p.UpdatedAt = p.CreatedAt
+
+	err = sess.InsertBySql(`INSERT INTO "portfolio" ("title", "uid", "author", "abstruct", "readme", "source", "link", "created_at", "updated_at") 
+	VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?) RETURNING "id"`,
+		p.Title, p.UID, p.Author, p.Abstruct, p.Readme, p.Source, p.Link, p.CreatedAt, p.UpdatedAt).Load(&p.ID)
+	if err != nil {
+		return Portfolio{}, err
+	}
 	return *p, nil
 }
 
 func putPortfolio(id int, p *Portfolio) (Portfolio, error) {
-
-	db, err := gormConnect()
+	sess, err := gocraftConnection()
 	if err != nil {
 		return Portfolio{}, err
 	}
-	defer db.Close()
-
-	p.UpdatedAt = time.Now()
-	p.ID = id
-	db.Save(p)
-	return *p, nil
+	portfolioMap := map[string]interface{}{
+		"title":      p.Title,
+		"author":     p.Author,
+		"abstruct":   p.Abstruct,
+		"readme":     p.Readme,
+		"source":     p.Source,
+		"link":       p.Link,
+		"updated_at": time.Now(),
+	}
+	_, err = sess.Update("portfolio").
+		SetMap(portfolioMap).
+		Where("id = ?", id).Exec()
+	if err != nil {
+		return Portfolio{}, err
+	}
+	return getPortfolio(id)
 }
 
 func deletePortfolio(id int) error {
-
-	db, err := gormConnect()
+	sess, err := gocraftConnection()
 	if err != nil {
 		return err
 	}
-	defer db.Close()
-	err = db.Delete(&Portfolio{}, id).Error
+	_, err = sess.DeleteFrom("portfolio").
+		Where("id = ?", id).Exec()
 	return err
 }

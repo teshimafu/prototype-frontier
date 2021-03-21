8c88b846753fdcfc7785f100ec6227ac616d3d60
teshimafucd <teshima.fu@gmail.com>
2021-03-22 02:53:15 +0900

    認証切れた時のエラーを表示するように修正

--- prototype-frontier-front/src/services/axiosConfig.ts
+++ prototype-frontier-front/src/services/axiosConfig.ts
@@ -6,19 +6,29 @@ export const axiosInstance: AxiosInstance = axios.create({
   baseURL: restAPIConfig.baseURL
 });
 
-export const axiosAuthInstance: Promise<AxiosInstance> = new Promise(
-  resolve => {
-    FirebaseService.getInstance()
-      .getIdToken()
-      .then(token =>
-        resolve(
-          axios.create({
-            baseURL: restAPIConfig.baseURL,
-            headers: {
-              Authorization: `Bearer ${token ?? ""}`
-            }
-          })
-        )
-      );
-  }
-);
+export const axiosAuthInstance: Promise<AxiosInstance> = new Promise<
+  AxiosInstance
+>(resolve => {
+  FirebaseService.getInstance()
+    .getIdToken()
+    .then(token =>
+      resolve(
+        axios.create({
+          baseURL: restAPIConfig.baseURL,
+          headers: {
+            Authorization: `Bearer ${token ?? ""}`
+          }
+        })
+      )
+    );
+}).then(instance => {
+  instance.interceptors.request.use(
+    r => r,
+    error => {
+      if (error.response.status === 401) {
+        alert("認証エラーです\n画面更新してやり直してください");
+      }
+    }
+  );
+  return instance;
+});
